eb23fe4521ba92b2eda3dd8884f3ad2a24d9d386
liubov1211 <lyubov12111994@gmail.com>
2021-11-22 23:45:46 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import logo from './img/logo.jpg';
+import facebook from './img/facebook.png';
+import imgBig from './img/imgBig.jpg';
 import './App.css';
 
 function App() {
+  const Author = {
+    name: "Anakin Skywalker",
+    photo: logo,
+    nickname: "@dart_vader",
+    content: "WTF? Who is Ray? Why she is Skywalker? Luke...?",
+    image: facebook,
+    date: "• 21 nov.",
+    imageBig: imgBig,
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={Author.photo} className="App-logo" alt="logo" />
+        <div className="App-block">
+          <h4> {Author.name} </h4>
+          <img src={Author.image} className="App-facebook" />
+          <span>{Author.nickname}</span>
+          <span> {Author.date}</span>
+          <p>{Author.content}</p>
+          <img src={Author.imageBig} className="ImageWomen" />
+        </div>
       </header>
+
     </div>
   );
 }
 
 export default App;
+
+
